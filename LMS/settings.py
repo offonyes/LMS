2d9a1e076eaec7ebc59f770702eb0ac5b6fdea2d
offonyes <dimkakatran@gmail.com>
2024-04-20 19:53:38 +0400

    update settings

--- LMS/settings.py
+++ LMS/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / 'media'  # new
-MEDIA_URL = '/media/'  # new
+MEDIA_ROOT = BASE_DIR / 'media'  
+MEDIA_URL = '/media/'  
